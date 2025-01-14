3914fa1654256058aa1fb0cd24062b71474225d6
Nishant Gupta <gupta.nishant08042004@gmail.com>
2025-01-14 22:52:47 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -35,7 +35,6 @@ const Header = () => {
             </Link>
           ))}
           <Link
-            href={}
             target="_blank"
             rel="noopener noreferrer"
             className="text-sm bg-lightSky/10 px-4 py-2 rounded-lg border border-hoverColor/10 hover:border-hoverColor hover:bg-hoverColor hover:text-black hoverEffect"
